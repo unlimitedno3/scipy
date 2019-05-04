636ca444f401e970d1a199e534275932760ddb8a
indra-ipd <indra.ipd@gmail.com>
2019-05-04 18:54:15 +0900

    Update _minimize.py
    Added my _minimize.py with myOptimizers

--- scipy/optimize/_minimize.py
+++ scipy/optimize/_minimize.py
@@ -18,25 +18,27 @@ import numpy as np
 
 from scipy._lib.six import callable
 
+from scipy.sparse.linalg import LinearOperator
+
 # unconstrained minimization
 from .optimize import (_minimize_neldermead, _minimize_powell, _minimize_cg,
-                       _minimize_bfgs, _minimize_newtoncg,
-                       _minimize_scalar_brent, _minimize_scalar_bounded,
+                       _minimize_obfgs,_minimize_onaq,_minimize_olbfgs,_minimize_olnaq,_minimize_adam,
+                       _minimize_adasecant,_minimize_adaQN,_minimize_adaNAQ,
+                       _minimize_newtoncg,_minimize_scalar_brent, _minimize_scalar_bounded,
                        _minimize_scalar_golden, MemoizeJac)
 from ._trustregion_dogleg import _minimize_dogleg
 from ._trustregion_ncg import _minimize_trust_ncg
-from ._trustregion_krylov import _minimize_trust_krylov
-from ._trustregion_exact import _minimize_trustregion_exact
-from ._trustregion_constr import _minimize_trustregion_constr
+#from ._trustregion_krylov import _minimize_trust_krylov
+#from ._trustregion_exact import _minimize_trustregion_exact
+#from ._trustregion_constr import _minimize_trustregion_constr
+#from ._constraints import Bounds, new_bounds_to_old, old_bound_to_new
+
 
 # constrained minimization
 from .lbfgsb import _minimize_lbfgsb
 from .tnc import _minimize_tnc
 from .cobyla import _minimize_cobyla
 from .slsqp import _minimize_slsqp
-from ._constraints import (old_bound_to_new, new_bounds_to_old,
-                           old_constraint_to_new, new_constraint_to_old,
-                           NonlinearConstraint, LinearConstraint, Bounds)
 
 
 def minimize(fun, x0, args=(), method=None, jac=None, hess=None,
@@ -205,6 +207,7 @@ def minimize(fun, x0, args=(), method=None, jac=None, hess=None,
         ``message`` which describes the cause of the termination. See
         `OptimizeResult` for a description of other attributes.
 
+
     See also
     --------
     minimize_scalar : Interface to minimization algorithms for scalar
@@ -348,7 +351,7 @@ def minimize(fun, x0, args=(), method=None, jac=None, hess=None,
     its contents also passed as `method` parameters pair by pair.  Also, if
     `jac` has been passed as a bool type, `jac` and `fun` are mangled so that
     `fun` returns just the function values and `jac` is converted to a function
-    returning the Jacobian.  The method shall return an `OptimizeResult`
+    returning the Jacobian.  The method shall return an ``OptimizeResult``
     object.
 
     The provided `method` callable must be able to accept (and possibly ignore)
@@ -537,8 +540,6 @@ def minimize(fun, x0, args=(), method=None, jac=None, hess=None,
                 jac = fun.derivative
             else:
                 jac = '2-point'
-        elif jac is None:
-            jac = '2-point'
         elif not callable(jac) and jac not in ('2-point', '3-point', 'cs'):
             raise ValueError("Unsupported jac definition.")
     else:
@@ -575,10 +576,13 @@ def minimize(fun, x0, args=(), method=None, jac=None, hess=None,
             options.setdefault('barrier_tol', tol)
 
     if bounds is not None:
-        bounds = standardize_bounds(bounds, x0, meth)
-
-    if constraints is not None:
-        constraints = standardize_constraints(constraints, x0, meth)
+        if meth == 'trust-constr':
+            if not isinstance(bounds, Bounds):
+                lb, ub = old_bound_to_new(bounds)
+                bounds = Bounds(lb, ub)
+        elif meth in ('l-bfgs-b', 'tnc', 'slsqp'):
+            if isinstance(bounds, Bounds):
+                bounds = new_bounds_to_old(bounds.lb, bounds.ub, x0.shape[0])
 
     if meth == '_custom':
         return method(fun, x0, args=args, jac=jac, hess=hess, hessp=hessp,
@@ -592,6 +596,42 @@ def minimize(fun, x0, args=(), method=None, jac=None, hess=None,
         return _minimize_cg(fun, x0, args, jac, callback, **options)
     elif meth == 'bfgs':
         return _minimize_bfgs(fun, x0, args, jac, callback, **options)
+    elif meth == 'mbfgs':
+        return _minimize_mbfgs(fun, x0, args, jac, callback, **options)
+    elif meth == 'naq':
+        return _minimize_naq(fun, x0, args, jac, callback, **options)
+    elif meth == 'onaq':
+        return _minimize_onaq(fun, x0, args, jac, callback, **options)
+    elif meth == 'obfgs':
+        return _minimize_obfgs(fun, x0, args, jac, callback, **options)
+
+    elif meth == 'lbfgs':
+        return _minimize_lbfgs(fun, x0, args, jac, callback, **options)
+    elif meth == 'olbfgs':
+        return _minimize_olbfgs(fun, x0, args, jac, callback, **options)
+    elif meth == 'molbfgs':
+        return _minimize_molbfgs(fun, x0, args, jac, callback, **options)
+    elif meth == 'lnaq':
+        return _minimize_lnaq(fun, x0, args, jac, callback, **options)
+    elif meth == 'molnaq':
+        return _minimize_molnaq(fun, x0, args, jac, callback, **options)
+    elif meth == 'olnaq':
+        return _minimize_olnaq(fun, x0, args, jac, callback, **options)
+    elif meth == 'solnaq':
+        return _minimize_solnaq(fun, x0, args, jac, callback, **options)
+
+    elif meth == 'adam':
+        return _minimize_adam(fun, x0, args, jac, callback, **options)
+    elif meth == 'adasecant':
+        return _minimize_adasecant(fun, x0, args, jac, callback, **options)
+    elif meth == 'nadasecant':
+        return _minimize_nes_adasecant(fun, x0, args, jac, callback, **options)
+
+    elif meth == 'adaqn':
+        return _minimize_adaQN(fun, x0, args, jac, callback, **options)
+    elif meth == 'adanaq':
+        return _minimize_adaNAQ(fun, x0, args, jac, callback, **options)
+
     elif meth == 'newton-cg':
         return _minimize_newtoncg(fun, x0, args, jac, hess, hessp, callback,
                                   **options)
@@ -712,7 +752,7 @@ def minimize_scalar(fun, bracket=None, bounds=None, args=(),
     where ``kwargs`` corresponds to any other parameters passed to `minimize`
     (such as `bracket`, `tol`, etc.), except the `options` dict, which has
     its contents also passed as `method` parameters pair by pair.  The method
-    shall return an `OptimizeResult` object.
+    shall return an ``OptimizeResult`` object.
 
     The provided `method` callable must be able to accept (and possibly ignore)
     arbitrary parameters; the set of parameters accepted by `minimize` may
@@ -782,38 +822,3 @@ def minimize_scalar(fun, bracket=None, bounds=None, args=(),
         return _minimize_scalar_golden(fun, bracket, args, **options)
     else:
         raise ValueError('Unknown solver %s' % method)
-
-
-def standardize_bounds(bounds, x0, meth):
-    """Converts bounds to the form required by the solver."""
-    if meth == 'trust-constr':
-        if not isinstance(bounds, Bounds):
-            lb, ub = old_bound_to_new(bounds)
-            bounds = Bounds(lb, ub)
-    elif meth in ('l-bfgs-b', 'tnc', 'slsqp'):
-        if isinstance(bounds, Bounds):
-            bounds = new_bounds_to_old(bounds.lb, bounds.ub, x0.shape[0])
-    return bounds
-
-
-def standardize_constraints(constraints, x0, meth):
-    """Converts constraints to the form required by the solver."""
-    all_constraint_types = (NonlinearConstraint, LinearConstraint, dict)
-    new_constraint_types = all_constraint_types[:-1]
-    if isinstance(constraints, all_constraint_types):
-        constraints = [constraints]
-    constraints = list(constraints)  # ensure it's a mutable sequence
-
-    if meth == 'trust-constr':
-        for i, con in enumerate(constraints):
-            if not isinstance(con, new_constraint_types):
-                constraints[i] = old_constraint_to_new(i, con)
-    else:
-        # iterate over copy, changing original
-        for i, con in enumerate(list(constraints)):
-            if isinstance(con, new_constraint_types):
-                old_constraints = new_constraint_to_old(con, x0)
-                constraints[i] = old_constraints[0]
-                constraints.extend(old_constraints[1:])  # appends 1 if present
-
-    return constraints
